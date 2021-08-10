8b16f2ba54baf4c887445e5f8227fe3b20854203
Lcchy <lucas@bonfida.com>
2021-08-10 18:58:17 +0200

    Rewrite critbit without type casting and unsafe code - untested

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -5,33 +5,33 @@ pub enum AgnosticOrderbookInstruction {
     /// 3. `[writable]` A zeroed out asks account
     /// 4. `[]` The rent sysvar system program
     /// 5. `[]` The market authority (optional)
-    InitializeMarket(InitializeMarketInstruction),
+    InitializeMarket,
     /// 0. `[writable]` The market account
     /// 1. `[writable]` The event queue account
     /// 2. `[writable]` The bids account
     /// 3. `[writable]` The asks account
     /// 4. `[]` The owner of the order
     /// 5. `[]` The rent sysvar system program
-    NewOrder(NewOrderInstruction),
+    NewOrder,
     /// 0. `[writable]` The market account
     /// 1. `[writable]` The event queue account
     /// 2. `[writable]` The bids account
     /// 3. `[writable]` The asks account
-    MatchOrders(u16),
+    MatchOrders,
     /// 0. `[writable]` The market account
     /// 1. `[writable]` The event queue account
     /// 2. `[signer]` The caller authority
-    ConsumeEvents(u16),
+    ConsumeEvents,
     /// 0. `[writable]` The market account
     /// 1. `[signer]` The order owner
     /// 2. `[writable]` Then asks or bids account
-    CancelOrder(CancelOrderInstruction),
+    CancelOrder,
     /// 0. `[writable]` The market account
     /// 1. `[writable]` The bids account
     /// 2. `[writable]` The asks account
     /// 3. `[signer]` The order owner
     /// 4. `[writable]` The event queue account
-    CancelOrderByClientId(u64),
+    CancelOrderByClientId,
     /// 0. `[writable]` The market account
     /// 1. `[signer]` The market authority
     DisableMarket,
