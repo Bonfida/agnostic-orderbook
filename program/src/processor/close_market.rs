7172910f920761dc31cc06a7d5861e7d5d7ab5ad
Lcchy <lcchy@mailbox.org>
2021-10-05 11:24:26 +0100

    Remove some debug remnant

--- program/src/processor/close_market.rs
+++ program/src/processor/close_market.rs
@@ -108,8 +108,5 @@ pub(crate) fn process(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramR
     **asks_lamports = 0;
     **event_queue_lamports = 0;
 
-    msg!("u128 : {:?}", std::mem::align_of_val(&1u128));
-    msg!("u64 : {:?}", std::mem::align_of_val(&1u128));
-
     Ok(())
 }
